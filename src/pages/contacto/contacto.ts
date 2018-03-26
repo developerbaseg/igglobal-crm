794cab86b28aac8a02b0a66e5e239b77b7a56134
Federico Valenzuela <fvalenzuela@baseglobal.com.ar>
2018-03-26 12:55:52 -0300

    contacto.ts
    Se agrego funcion seleccion CoCargo y CoDepartamento.

--- src/pages/contacto/contacto.ts
+++ src/pages/contacto/contacto.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { ContactosDataProvider } from '../../providers/contactos-data/contactos-data';
 import { CargosDataProvider } from '../../providers/cargos-data/cargos-data';
-//import { SelectorPage } from '../../pages/selector/selector';
+import { DepartamentosDataProvider } from '../../providers/departamentos-data/departamentos-data';
 import { S2Component } from "../../component/s2.component";
 import { s2SelectorShowResponse } from '../../interfaces/s2-selector-options';
 
@@ -21,10 +21,11 @@ export class ContactoPage extends S2Component{
   profile:{};
   model: any;
   dataCoCargo: any[] =[];
+  dataCoDepartamento: any[] =[];
   fecha: any;
   users: any[] = [];
   contacto: any = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams, public contactosProvider: ContactosDataProvider, public cargosProvider: CargosDataProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public contactosProvider: ContactosDataProvider, public cargosProvider: CargosDataProvider, public departamentosProvider: DepartamentosDataProvider) {
     super(navCtrl);
     this.model = {
       // NoInterno: null,
@@ -39,17 +40,26 @@ export class ContactoPage extends S2Component{
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactoPage');
+    //console.log('ionViewDidLoad ContactoPage');
   }
   //--------------ionViewWillEnter()--------------
-  //--Trae todos los Nointernos de CuentaDataProvider--
+  //Trae todos los datos de CoCargo y CoDepartamento
   ionViewWillEnter() {
     this.cargosProvider.allAsync()
       .subscribe(
         (response) => {
-          console.log(response);
+          //console.log(response);
           this.dataCoCargo = response.data;
-
+        },
+        (error) => {
+          console.error(error);
+        }
+      )
+    this.departamentosProvider.allAsync()
+      .subscribe(
+        (response) => {
+          //console.log(response);
+          this.dataCoDepartamento = response.data;
         },
         (error) => {
           console.error(error);
@@ -89,8 +99,8 @@ export class ContactoPage extends S2Component{
     console.log('armar el DTO que espera el servidor -> copiarse el formato que envia el POST del IGGLOBAL Online')
     this.contactosProvider.saveAsync();
   }
-  //-------------Select CoCargo---------------
-  seleccionCoCargo() {
+  //-------------seleccionCoCargo()---------------
+  seleccionCargo() {
     this.s2ShowSelector({
       title: 'Co. Cargo',
       id: 'CoCargo',
@@ -102,7 +112,23 @@ export class ContactoPage extends S2Component{
     .then((response: s2SelectorShowResponse) => {
       console.log(response);
       this.model.CoCargoSe = response.id;
-      this.model.NoInternoDescripcion = response.descripcion;
+      this.model.CoCargoDescripcion = response.descripcion;
+    })
+  }
+  //-------------seleccionDepartamento()---------------
+  seleccionDepartamento() {
+    this.s2ShowSelector({
+      title: 'Co. Departamento',
+      id: 'CoDepartamento',
+      descripcion: 'Departamento',
+      selectedValue: this.model.CoDepartamentoSe,
+      options: this.dataCoDepartamento,
+      //useGroups: true
+    })
+    .then((response: s2SelectorShowResponse) => {
+      console.log(response);
+      this.model.CoDepartamentoSe = response.id;
+      this.model.CoDepartamentoDescripcion = response.descripcion;
     })
   }
 
